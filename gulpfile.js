c026895f37146faad3958137ac815f467932eaa4
Louie Babb <louiebabb@Louies-MacBook-Pro.local>
2017-10-29 11:21:57 +0000

    Gulp set up

--- gulpfile.js
+++ gulpfile.js
@@ -4,59 +4,79 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     notify = require('gulp-notify'),
     clean = require('gulp-clean'),
-    fileinclude = require('gulp-file-include');
+    fileinclude = require('gulp-file-include')
+    gulpCopy = require('gulp-copy');
+
+
+var sourceFilesFonts = 'src/assets/fonts/*',
+    destinationFonts = 'dist/fonts/',
+    sourceFilesHtml = ['src/html/pages/*.html','src/html/includes/*.html'],
+    destinationHtml = 'dist/',
+    sourceFilesJs = 'src/assets/scripts/*.js',
+    destinationJs = 'dist/scripts',
+    sourceFilesCSS = 'src/assets/sass/main.scss',
+    destinationCSS = 'dist/styles';
 
 
 // HTML Partials
-gulp.task('fileinclude', () =>
-  gulp.src(['src/html/pages/index.html'])
+gulp.task('htmlbuild', () =>
+  gulp.src('src/html/pages/*.html')
     .pipe(fileinclude({
       prefix: '@@',
       basepath: '@file'
     }))
-    .pipe(gulp.dest('dist/'))
+    .pipe(gulp.dest(destinationHtml))
     .pipe(notify({ message: 'HTML task complete' }))
 );
 
 //Styles
 gulp.task('styles', () =>
-  sass('src/assets/sass/main.scss')
+  sass(sourceFilesCSS)
     .on('error', sass.logError)
     .pipe(cssnano())
-    .pipe(gulp.dest('dist/assets/styles'))
+    .pipe(gulp.dest(destinationCSS))
     .pipe(notify({ message: 'Styles task complete' }))
 );
 
 gulp.task('scripts', () =>
-  gulp.src('src/assets/scripts/*.js')
+  gulp.src(sourceFilesJs)
     .pipe(concat('main.js'))
-    .pipe(gulp.dest('./dist/assets/js'))
+    .pipe(gulp.dest(destinationJs))
     .pipe(notify({ message: 'Scripts task complete' }))
+);
+
+//Fonts copy
+gulp.task('fonts', () =>
+  gulp.src(sourceFilesFonts)
+    .pipe(gulp.dest(destinationFonts))
+    .pipe(notify({ message: 'Fonts task complete' }))
 );
 
 //Clean
 gulp.task('clean', () =>
-  gulp.src('dist/assets/styles/*.css', {read: false})
+  gulp.src([destinationCSS, destinationJs, 'dist/images' , destinationFonts ], {read: false})
     .pipe(clean())
 );
 
 gulp.task('default', ['clean'], function() {
-    gulp.start('styles');
+    gulp.start(['htmlbuild','styles','scripts','fonts']);
 });
 
-gulp.task('watch', () =>
+gulp.task('watch', function() {
 
   // Watch .scss files
   gulp.watch('src/assets/sass/**/*.scss', ['styles'])
   gulp.watch('src/assets/sass/*.scss', ['styles'])
 
   // Watch .js files
-  gulp.watch('src/assets/scripts/*.js', ['scripts'])
+  gulp.watch(sourceFilesJs, ['scripts'])
 
   //Watch Html files
-  gulp.watch('src/html/pages/*.html', ['fileinclude'])
-  gulp.watch('src/html/includes/*.html', ['fileinclude'])
+  gulp.watch(sourceFilesHtml, ['htmlbuild'])
+
+  //Watch Font files
+  gulp.watch(sourceFilesFonts, ['fonts'])
 
   // Watch image files
   // gulp.watch('src/assetsimages/**/*', ['images']);
-);
+});
